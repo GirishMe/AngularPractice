12c42e5cd860bbe1915cd003459243a968d6ee14
Girish <girishmehata77@gmail.com>
2022-02-23 22:43:23 +0530

    Added new Component in MyPractice

--- MyAngularPractice/src/app/app.module.ts
+++ MyAngularPractice/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ProductDetailsComponent, HeaderComponent, FooterComponent, FrontPageComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component:FrontPageComponent ,
+      },
+      {
+        path: 'product',
+        component: ProductDetailsComponent,
+      },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
